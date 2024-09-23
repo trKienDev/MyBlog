2d11312d5dfa4951a5e371da44351a8a1a13f255
trKienDev <trkien.dev@gmail.com>
2024-09-23 17:31:08 +0700

    Update & fix bug

--- backend/src/Admin/Controllers/admin.controller.ts
+++ backend/src/Admin/Controllers/admin.controller.ts
@@ -1,16 +1,16 @@
 import { IncomingMessage, ServerResponse } from "http";
 import * as path from 'path';
 import * as fs from 'fs';
+import Admin from '../../models/pages/adminPage.model';
 
 // Controller để xử lý route / About
 export const getAdminPage = (req: IncomingMessage, res: ServerResponse) => {
-        const aboutData = {
+        const adminData = {
                 title : "Admin",
         };
-
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify(aboutData));
+        res.end(JSON.stringify(adminData));
 };
 
 
