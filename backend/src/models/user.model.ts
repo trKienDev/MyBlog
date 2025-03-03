e78412e6d8393e52bfc0075b2cd08429d9e3a3fc
trKienDev <trkien.dev@gmail.com>
2025-03-03 17:15:33 +0700

    update

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -2,8 +2,8 @@ import mongoose, { Schema, Document } from "mongoose";
  
 // Define Schema
 const userSchema: Schema = new Schema({
-        username: { type: String, required: true },
-        password: { type: String, required: true },
+      username: { type: String, required: true },
+      password: { type: String, required: true },
 });
 
 // Create Model from Schema
@@ -11,8 +11,8 @@ const User = mongoose.model<UserDocument>('User', userSchema);
 
 // Interface model in MongoDB
 interface UserDocument extends Document {
-        username: string;
-        password: string;
+      username: string;
+      password: string;
 }
 
 export default User;
--- backend/src/routes/admin.route.ts
+++ backend/src/routes/admin.route.ts
@@ -13,105 +13,103 @@ import { CustomRequest } from "../interfaces/CustomRequest.js";
 import { attachParams } from '../middlewares/attachParams.js';
 
 export const adminRoutes = (req: IncomingMessage, res: ServerResponse) => {
-        // Extract the URL and method from the request
-        const { url, method } = req;
+      // Extract the URL and method from the request
+      const { url, method } = req;
 
-        // pages
-        if ( url === '/admin' && method === 'GET' ) {
-                getAdminPage(req, res);
-        } else if ( url === '/about' && method === 'GET' ) {
-                getAboutPage(req, res);
-        } 
-        // sidebar
-        else if ( url === '/admin/sidebar/read' && method === 'GET' ) { // Route for getting all sidebar items
-                getSidebarItems ( req, res );
-        } else if ( url === '/admin/sidebar/create' && method === 'POST' ) { // Route for creating a new sidebar item
-                createSidebarItem ( req, res );
-        } else if ( url?.startsWith ( '/admin/sidebar/update' ) && method === 'PUT' ) {
-                updateSidebarItem ( req, res );
-        } else if ( url?.startsWith ( '/admin/sidebar/delete' ) && method === 'DELETE' ) {
-                deleteSidebarItem ( req, res );
-        } 
-        // actress 
-        else if ( url?.startsWith ( '/admin/actress/read' ) && method === 'GET' ) {
-                getActress ( req , res );
-        } else if ( url?.startsWith ( '/admin/actress/create' ) && method === 'POST' ) {
-                createActress ( req as CustomRequest, res );
-        } else if ( url?.startsWith ( '/admin/actress/update' ) && method === 'PUT' ) {
-                updateActress ( req as CustomRequest, res );
-        } else if ( url?.startsWith ( '/admin/actress/delete' ) && method === 'DELETE' ) {
-                deleteActress ( req , res ); 
-        }
-        // studio
-        else if ( url?.startsWith ( '/admin/studio/read' ) && method === 'GET' ) {
-                getStudio ( req as CustomRequest, res );
-        } else if ( url?.startsWith ( '/admin/studio/create' ) && method === 'POST' ) {
-                createStudio ( req as CustomRequest, res );
-        } else if ( url?.startsWith ( '/admin/studio/update' ) && method === 'PUT' ) {
-                updateStudio ( req as CustomRequest, res );
-        } else if ( url?.startsWith ( '/admin/studio/delete' ) && method === 'DELETE' ) {
-                deleteStudio ( req , res ); 
-        }
-        // codeAV
-        else if ( url?.startsWith ( '/admin/codeAV/read' ) && method === 'GET' ) {
-                getCodeAV ( req , res );
-        } else if ( url?.startsWith ( '/admin/codeAV/create' ) && method === 'POST' ) {
-                createCodeAV ( req as CustomRequest, res );
-        } else if ( url?.startsWith ( '/admin/codeAV/update' ) && method === 'PUT' ) {
-                updateCodeAV ( req as CustomRequest, res );
-        } else if ( url?.startsWith ( '/admin/codeAV/delete' ) && method === 'DELETE' ) {
-                deleteCodeAV ( req , res ); 
-        }
-        // Tags
-        else if ( url?.startsWith ( '/admin/tags/read' ) && method === 'GET' ) {
-                getTags (req, res);
-        } else if ( url?.startsWith ( '/admin/tags/video_read' ) && method === 'GET' ) {
-                getTagVideo (req, res);
-        } else if ( url?.startsWith ( '/admin/tags/create' ) && method === 'POST' ) {
-                createTag ( req, res );
-        } else if ( url?.startsWith ( '/admin/tags/update' ) && method === 'PUT' ) {
-                updateTag ( req, res );
-        } else if ( url?.startsWith ( '/admin/tags/delete' ) && method === 'DELETE' ) {
-                deleteTag ( req , res ); 
-        }
-        // Video
-        else if ( url?.startsWith ( '/admin/video/create' ) && method === 'POST' ) {
-                createVideo (req, res);
-        } else if ( url?.startsWith ( '/admin/video/get_video_by_id' ) && method === 'GET' ) {
-                getVideoById (req as CustomRequest, res);
-        } else if ( url?.startsWith ( '/admin/video/update' ) && method === 'PUT' ) {
-                updateVideo ( req as CustomRequest, res );
-        } else if ( url?.startsWith ( '/admin/video/delete' ) && method === 'DELETE' ) {
-                deleteVideo (req, res);
-        }
-        // Films
-        else if ( url?.startsWith ( '/admin/film/create' ) && method === 'POST' ) {
-                createFilm (req as CustomRequest, res);
-        } else if ( url?.startsWith ( '/admin/film/read' ) && method === 'GET' ) {
-                getFilm (req, res);
-        } else if ( url?.startsWith ( '/admin/film/update' ) && method === 'PUT' ) {
-                updateFilm (req as CustomRequest, res);
-        } else if ( url?.startsWith ( '/admin/film/delete' ) && method === 'DELETE' ) {
-                deleteFilm (req as CustomRequest, res);
-        } 
-        // Story
-        else if ( url?.startsWith ( '/admin/story/create' ) && method === 'POST' ) {
-                createStory ( req, res );
-        } else if ( url?.startsWith ( '/admin/story/read' ) && method === 'GET' ) {
-                getStory ( req, res );
-        } else if ( url?.startsWith ( '/admin/story/update' ) && method === 'PUT' ) {
-                updateStory ( req, res );
-        } else if ( url?.startsWith ( '/admin/story/delete' ) && method === 'DELETE' ) {
-                deleteStory ( req, res );
-        } 
+      // pages
+      if ( url === '/admin' && method === 'GET' ) {
+            getAdminPage(req, res);
+      } else if ( url === '/about' && method === 'GET' ) {
+            getAboutPage(req, res);
+      } 
+      // sidebar
+      else if ( url === '/admin/sidebar/read' && method === 'GET' ) { // Route for getting all sidebar items
+            getSidebarItems ( req, res );
+      } else if ( url === '/admin/sidebar/create' && method === 'POST' ) { // Route for creating a new sidebar item
+            createSidebarItem ( req, res );
+      } else if ( url?.startsWith ( '/admin/sidebar/update' ) && method === 'PUT' ) {
+            updateSidebarItem ( req, res );
+      } else if ( url?.startsWith ( '/admin/sidebar/delete' ) && method === 'DELETE' ) {
+            deleteSidebarItem ( req, res );
+      } 
+      // actress 
+      else if ( url?.startsWith ( '/admin/actress/read' ) && method === 'GET' ) {
+            getActress ( req , res );
+      } else if ( url?.startsWith ( '/admin/actress/create' ) && method === 'POST' ) {
+            createActress ( req as CustomRequest, res );
+      } else if ( url?.startsWith ( '/admin/actress/update' ) && method === 'PUT' ) {
+            updateActress ( req as CustomRequest, res );
+      } else if ( url?.startsWith ( '/admin/actress/delete' ) && method === 'DELETE' ) {
+            deleteActress ( req , res ); 
+      }
+      // studio
+      else if ( url?.startsWith ( '/admin/studio/read' ) && method === 'GET' ) {
+            getStudio ( req as CustomRequest, res );
+      } else if ( url?.startsWith ( '/admin/studio/create' ) && method === 'POST' ) {
+            createStudio ( req as CustomRequest, res );
+      } else if ( url?.startsWith ( '/admin/studio/update' ) && method === 'PUT' ) {
+            updateStudio ( req as CustomRequest, res );
+      } else if ( url?.startsWith ( '/admin/studio/delete' ) && method === 'DELETE' ) {
+            deleteStudio ( req , res ); 
+      }
+      // codeAV
+      else if ( url?.startsWith ( '/admin/codeAV/read' ) && method === 'GET' ) {
+            getCodeAV ( req , res );
+      } else if ( url?.startsWith ( '/admin/codeAV/create' ) && method === 'POST' ) {
+            createCodeAV ( req as CustomRequest, res );
+      } else if ( url?.startsWith ( '/admin/codeAV/update' ) && method === 'PUT' ) {
+            updateCodeAV ( req as CustomRequest, res );
+      } else if ( url?.startsWith ( '/admin/codeAV/delete' ) && method === 'DELETE' ) {
+            deleteCodeAV ( req , res ); 
+      }
+      // Tags
+      else if ( url?.startsWith ( '/admin/tags/read' ) && method === 'GET' ) {
+            getTags (req, res);
+      } else if ( url?.startsWith ( '/admin/tags/video_read' ) && method === 'GET' ) {
+            getTagVideo (req, res);
+      } else if ( url?.startsWith ( '/admin/tags/create' ) && method === 'POST' ) {
+            createTag ( req, res );
+      } else if ( url?.startsWith ( '/admin/tags/update' ) && method === 'PUT' ) {
+            updateTag ( req, res );
+      } else if ( url?.startsWith ( '/admin/tags/delete' ) && method === 'DELETE' ) {
+            deleteTag ( req , res ); 
+      }
+      // Video
+      else if ( url?.startsWith ( '/admin/video/create' ) && method === 'POST' ) {
+            createVideo (req, res);
+      } else if ( url?.startsWith ( '/admin/video/get_video_by_id' ) && method === 'GET' ) {
+            getVideoById (req as CustomRequest, res);
+      } else if ( url?.startsWith ( '/admin/video/update' ) && method === 'PUT' ) {
+            updateVideo ( req as CustomRequest, res );
+      } else if ( url?.startsWith ( '/admin/video/delete' ) && method === 'DELETE' ) {
+            deleteVideo (req, res);
+      }
+      // Films
+      else if ( url?.startsWith ( '/admin/film/create' ) && method === 'POST' ) {
+            createFilm (req as CustomRequest, res);
+      } else if ( url?.startsWith ( '/admin/film/read' ) && method === 'GET' ) {
+            getFilm (req, res);
+      } else if ( url?.startsWith ( '/admin/film/update' ) && method === 'PUT' ) {
+            updateFilm (req as CustomRequest, res);
+      } else if ( url?.startsWith ( '/admin/film/delete' ) && method === 'DELETE' ) {
+            deleteFilm (req as CustomRequest, res);
+      } 
+      // Story
+      else if ( url?.startsWith ( '/admin/story/create' ) && method === 'POST' ) {
+            createStory ( req, res );
+      } else if ( url?.startsWith ( '/admin/story/read' ) && method === 'GET' ) {
+            getStory ( req, res );
+      } else if ( url?.startsWith ( '/admin/story/update' ) && method === 'PUT' ) {
+            updateStory ( req, res );
+      } else if ( url?.startsWith ( '/admin/story/delete' ) && method === 'DELETE' ) {
+            deleteStory ( req, res );
+      } 
 
-        else { // Handle invalid routes
-                res.statusCode = 404;
-                res.setHeader('Content-Type', 'text/plain');
-                res.end('Admin Route Not Found');
-        }
-
-
+      else { // Handle invalid routes
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'text/plain');
+            res.end('Admin Route Not Found');
+      }
 }
 
 export default adminRoutes;
